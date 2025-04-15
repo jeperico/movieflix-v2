a94e8221f35a71f4196f974576a8112cdda2e72a
João Périco <joaoeperico@gmail.com>
2025-04-15 14:41:33 +0000

    [style] remove unused include and spacing

--- main.c
+++ main.c
@@ -3,12 +3,11 @@
 #include "utils/render_table.h"
 
 void main() {
-    char title[101];
     int type, raiting, genre, provider;
     // [TYPE]       => (1 filme, 2 série, 3 documentário)
     // [RAITING]    => (0 livre, 10 10+, 18 18+)
     // [GENRE]      => (1 ação, 2 comédia, 3 drama, 4 suspense)
-    // [PROVIDER]  => (1 Netflix, 2 Amazon Prime, 3 Disney+, 4 HBO Max)
+    // [PROVIDER]   => (1 Netflix, 2 Amazon Prime, 3 Disney+, 4 HBO Max)
 
     renderMoviesTable();
 
