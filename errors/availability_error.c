43094bdb427a30404baabee73fe4cde38e2e1204
jeperico <joaoeperico@gmail.com>
2025-04-15 21:49:35 -0300

    [style] add final file line

--- errors/availability_error.c
+++ errors/availability_error.c
@@ -2,5 +2,5 @@
 #include "availability_error.h"
 
 void availabilityError() {
-    printf("[AVAILABILITY ERROR]\nOps! Nenhuma opção disponível\n[NEXT CATALOG] 20/02/2002 - 20:02:20\n");
+    printf("\n[AVAILABILITY ERROR]\nOps! Nenhuma opção disponível\n[NEXT CATALOG] 20/02/2002 - 20:02:20\n\n\n");
 }
